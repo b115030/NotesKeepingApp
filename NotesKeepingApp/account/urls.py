a12f8b548eb9627ade878c486c0d53a108f7c271
b115030 <iitiangokul@gmail.com>
2020-12-08 07:54:42 +0530

    [Gopinath]Added: signup page for user registration

--- NotesKeepingApp/account/urls.py
+++ NotesKeepingApp/account/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.urls import path, include
 from .api import RegisterApi
+from . import views
 
 urlpatterns = [
       path('api/register', RegisterApi.as_view()),
+      path('signup/', views.SignUp.as_view(), name='signup'),
 ]
